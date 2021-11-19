89966c65f8def1ef27cc269d7ced31170b5cfb93
Frostypine <frostypinekng@yahoo.com>
2021-11-19 00:54:28 -0700

    updating contact page

--- src/App/components/Contact.js
+++ src/App/components/Contact.js
@@ -1,4 +1,19 @@
 import './Components.css';
-const Contact = () => { return (<p className = "placeholdertext"> this is contact </p>); }
+const Contact = () => { return (
+<form>
+<p className = "placeholdertext"> this is contact </p>
+<label> 
+    email:
+<input/>
+</label>
+<br />
+<label> 
+    message: 
+<input/>
+</label>
+<br />
+<button>Submit</button>
+</form>
+); }
 
 export default Contact; 
