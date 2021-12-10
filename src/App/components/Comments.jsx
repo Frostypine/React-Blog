ca82f14a4ccf0a6ccae690d993f08c8d0041891a
Frostypine <frostypinekng@yahoo.com>
2021-12-10 10:01:59 -0700

    starting comments

---
+++ src/App/components/Comments.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Button, Form, Accordion, Card } from 'react-bootstrap';
+import { useDispatch,useSelector } from 'react-redux';
+import { addComments, selectComments } from '../../state/store/commentSlice';
+
+const Comments = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Comments; 
--- src/state/store/store.js
+++ src/state/store/store.js
@@ -5,9 +5,11 @@
 
 import {configureStore } from '@reduxjs/toolkit';
 import contentReducer from '../contentSlice'; 
+import commentReducer from '../commentSlice';
 
 export const store = configureStore({
     reducer:{
         content: contentReducer,
+        comment: commentReducer,
     }
 });
